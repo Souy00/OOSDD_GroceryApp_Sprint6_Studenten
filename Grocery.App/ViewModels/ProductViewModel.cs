502aa8399970c77a994ed4e949f75ba84d25b6e9
Souy00 <youssef_r12@hotmail.com>
2025-10-24 22:07:42 +0200

    USC 18 werkt 19 niet

--- Grocery.App/ViewModels/ProductViewModel.cs
+++ Grocery.App/ViewModels/ProductViewModel.cs
@@ -1,19 +1,48 @@
 ﻿using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
 
 namespace Grocery.App.ViewModels
 {
-    public class ProductViewModel : BaseViewModel
+    public partial class ProductViewModel : BaseViewModel
     {
         private readonly IProductService _productService;
+
+        // ObservableCollection van Product
         public ObservableCollection<Product> Products { get; set; }
 
+        // Command voor toevoegen nieuw product
+        public ICommand AddProductCommand { get; }
+
+        // Constructor
         public ProductViewModel(IProductService productService)
         {
             _productService = productService;
-            Products = [];
-            foreach (Product p in _productService.GetAll()) Products.Add(p);
+
+            // Haal alle producten op en voeg toe aan ObservableCollection
+            Products = new ObservableCollection<Product>(_productService.GetAll());
+
+            // Command instellen
+            AddProductCommand = new Command(async () =>
+            {
+                // Navigeer naar NewProductView
+                await Shell.Current.GoToAsync(nameof(Views.NewProductView));
+
+                // Lijst refreshen na terugkomst
+                RefreshProducts();
+            });
+        }
+
+        // Hulpmethode om Products te refreshen
+        private void RefreshProducts()
+        {
+            Products.Clear();
+            foreach (var p in _productService.GetAll())
+            {
+                Products.Add(p);
+            }
         }
     }
 }
--- Grocery.Core/Models/Product.cs
+++ Grocery.Core/Models/Product.cs
@@ -7,19 +7,27 @@ namespace Grocery.Core.Models
     {
         [ObservableProperty]
         public int stock;
+
         public DateOnly ShelfLife { get; set; }
+
         [RegularExpression(@"^\d+\.\d{0,2}$")]
         [Range(0, 999.99)]
-        public Decimal Price { get; set; } = 0;
-        public Product(int id, string name, int stock) : this(id, name, stock, default, 0) { }
-        public Product(int id, string name, int stock, DateOnly shelfLife) : this(id, name, stock, default, 0) { }
+        public decimal Price { get; set; } = 0;
 
-        public Product(int id, string name, int stock, DateOnly shelfLife, Decimal price) : base(id, name)
+        public Product(int id, string name, int stock)
+            : this(id, name, stock, default, 0) { }
+
+        public Product(int id, string name, int stock, DateOnly shelfLife)
+            : this(id, name, stock, shelfLife, 0) { }
+
+        public Product(int id, string name, int stock, DateOnly shelfLife, decimal price)
+            : base(id, name)
         {
             Stock = stock;
             ShelfLife = shelfLife;
             Price = price;
         }
+
         public override string? ToString()
         {
             return $"{Name} - {Stock} op voorraad";
